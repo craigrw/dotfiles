b8bfc5a8fa3548acc0e231488c38e779a9732bf8
Craig Warren <craigrwarren@gmail.com>
2017-03-12 18:38:26 -0700

    Set background to screensaver

--- .hammerspoon/caffeine.lua
+++ .hammerspoon/caffeine.lua
@@ -1,16 +1,24 @@
-
+local logger = hs.logger.new('caffeine', 'debug')
 
 local caffeine = hs.menubar.new()
 function setCaffeineDisplay(state)
   if state then
-    caffeine:setTitle("AWAKE")
+    caffeine:setTitle("☕")
   else
-    caffeine:setTitle("SLEEPY")
+    caffeine:setTitle("😴")
   end
 end
 
 function caffeineClicked()
   setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+
+function setBackground()
+  os.execute("/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background &")
+end
+
+function resetBackground()
+  os.execute("pkill ScreenSaverEngine")
 end
 
 local mod = {}
@@ -21,6 +29,15 @@ function mod.init()
     hs.caffeinate.set("acAndBattery", false)
     setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
   end
+
+  hs.caffeinate.watcher.new(function(event)
+    if event == hs.caffeinate.watcher.systemWillSleep then
+      setBackground()
+    elseif event == hs.caffeinate.watcher.systemDidWake then
+      resetBackground()
+    end
+  end):start()
+  setBackground()
 end
 
 return mod
